92f4c4c529dc5808e3807c3f6b919efdd84828ae
ImronDaramae <imrondaramae@gmail.com>
2024-07-20 00:04:52 +0700

    Init Vue Vuetify

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<VApp>
+            <VAppBar app>
+                <VToolbarTitle>My Nuxt App</VToolbarTitle>
+            </VAppBar>
+			<VMain>
+				<slot />
+			</VMain>
+		</VApp>
+	</div>
+</template>
